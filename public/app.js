b1c2fe4568390002178769305f8d856f2446d475
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-07-17 19:37:58 -0500

    added button to scrape articles

--- public/app.js
+++ public/app.js
@@ -1,15 +1,23 @@
-$.getJSON("/articles", function(data) {
-  for (var i = 0; i < data.length; i++) {
-    $("#articles").append(
-      "<p data-id='" +
-        data[i]._id +
-        "'>" +
-        data[i].title +
-        "<br />" +
-        data[i].link +
-        "</p>"
-    );
-  }
+$(document).on("click", "#scrape", function() {
+  $("#scrape").hide();
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  }).then(function() {
+    $.getJSON("/articles", function(data) {
+      for (var i = 0; i < data.length; i++) {
+        $("#articles").append(
+          "<p data-id='" +
+            data[i]._id +
+            "'>" +
+            data[i].title +
+            "<br />" +
+            data[i].link +
+            "</p>"
+        );
+      }
+    });
+  });
 });
 
 $(document).on("click", "p", function() {
